1e046bf507b2807f1b0f617c0550af7c861139fb
jih5663 <jih9418@email.daelim.co.kr>
2021-09-15 12:52:03 +0900

    3주차 수업

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import Potato from "./Potato";
 
+const foodLike = [
+  {
+    name:'chikin',
+    image:'http://jangdokfood.com/wp-content/uploads/2019/05/%EC%96%91%EB%85%90%EC%B9%98%ED%82%A8-768x496.jpg'
+  },
+
+  {
+    name:'ham',
+    image:'https://cdn.eyesmag.com/content/uploads/posts/2020/09/28/fast-food-franchises-serve-hamburger-without-tomatoes-01-52f4f2a6-a73a-4abc-ba3c-419ee2660639.jpg'
+
+  }
+ 
+]
+
+function renderFood(foo) {
+  return <Food name={foo.name} picture={foo.image}/>
+}
+
+
 function App() {
   return (
     <div>
-      <h1>Hello React!!!</h1>
-      <Potato/>
+      {
+        foodLike.map(renderFood)
+      }
+
+    </div>
+  );
+}
+
+function Food({name,picture}) {
+  //console.log(props);
+  return(
+    <div>
+    <h3>I love {name}</h3>
+    <img src={picture}/>
     </div>
   );
 }
